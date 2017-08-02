fb0a71bfa5d80648699f87e62128c5e97a570f4b
Rade Kuruc <radekuruc@gmail.com>
2017-08-01 20:31:35 -0400

    wip

---
+++ web/js/rsInit.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+        $(".royalSlider").royalSlider({
+            // options go here
+            // as an example, enable keyboard arrows nav
+            keyboardNavEnabled: true,
+            imageScaleMode: 'fill',
+            controlNavigation: 'none'
+        });  
+    });
